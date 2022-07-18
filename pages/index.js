0f6eca27e0ce49ba5aa38c9ae597cf2ed4897da7
Nnamdi <nnamdi.b@gmail.com>
2022-07-18 19:42:39 +0200

    get files from posts dir & create slug

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head';
+import path from 'path';
+import fs from 'fs';
 
-export default function HomePage() {
+export default function HomePage(props) {
+  const { posts } = props;
+
   return (
     <div>
       <Head>
@@ -10,4 +14,28 @@ export default function HomePage() {
       <h1>Hello World</h1>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  // fs reads from root level, so we can go directly in to posts
+  // Get files from the posts dir
+  const files = fs.readdirSync(path.join('posts'));
+
+  // Get slug and frontmatter from posts
+  const posts = files.map((filename) => {
+    // Create slug
+    const slug = filename.replace('.md', '');
+    return {
+      slug,
+    };
+  });
+
+  console.log(posts);
+
+  console.log(files);
+  return {
+    props: {
+      posts: 'The Posts',
+    },
+  };
 }
---
+++ posts/test.md
@@ -0,0 +1,10 @@
+---
+title: "Test Post"
+date: "July 18 2022"
+excerpt: "This is the excerpt"
+cover_image: "/images/posts/img1.jpg"
+---
+## Heading Two
+
+* Item 1
+* Item 2
