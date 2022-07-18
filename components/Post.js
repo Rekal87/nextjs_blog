763a9f6cd97531517d2b1ee3d4a3264631229805
Nnamdi <nnamdi.b@gmail.com>
2022-07-18 22:10:28 +0200

    filled in more of the Post component

--- components/Post.js
+++ components/Post.js
@@ -4,12 +4,24 @@ import Image from 'next/image';
 export default function Posts({ post }) {
   return (
     <div className='card'>
-      <Image
-        src={post.frontmatter.cover_image}
-        alt={post.frontmatter.cover_image}
-        width={150}
-        height={150}
-      />
+      <div className='card-img'>
+        <Image
+          src={post.frontmatter.cover_image}
+          alt={post.frontmatter.cover_image}
+          layout='fill'
+        />
+      </div>
+      <div className='card-body'>
+        <div className='card-title'>{post.frontmatter.title}</div>
+        <div className='card-text'>{post.frontmatter.excerpt}</div>
+      </div>
+
+      <div className='post-date'>Posted on {post.frontmatter.date}</div>
+      <Link href={`/blog/${post.slug}`}>
+        <a href='' className='btn'>
+          test
+        </a>
+      </Link>
     </div>
   );
 }
